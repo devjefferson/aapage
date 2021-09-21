768b1406c578f6ad3634ff27dbe66fbfce0ffa9b
Jefferson Fonseca <drpcscan@gmail.com>
2021-09-21 16:39:28 -0300

    connection the google analytic

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,23 @@
 import type { AppProps } from 'next/app'
 import GlobalStyle from '../styles/global'
+import { useRouter } from 'next/dist/client/router';
+import { useEffect } from 'react';
+import *  as gtag from '../lib/gtag';
+const isProduction = process.env.NODE_ENV === "production";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(()=>{
+    const handleRouteChange = (url: URL) => {
+      /* invoke analytics function only for production */
+      if (isProduction) gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  },[router.events])
   return (
   <>
     <GlobalStyle />
