b39dccb459446a7576a197f80ae50e74ffc8d13b
Jefferson Fonseca <drpcscan@gmail.com>
2021-09-27 12:08:13 -0300

    add first tag maneger google analytic

--- src/lib/gtag.ts
+++ src/lib/gtag.ts
@@ -1,1 +1,23 @@
 export const GA_TRACKING_ID = "G-WT5ZHZKW59";
+
+export const pageview = (url: URL) => {
+  window.gtag("config", GA_TRACKING_ID, {
+    page_path: url
+  }) 
+};
+
+type GTagEvent = {
+  action: string;
+  category: string;
+  label: string;
+  value: number;
+};
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/events
+export const event = ({ action, category, label, value }: GTagEvent) => {
+  window.gtag("event", action, {
+    event_category: category,
+    event_label: label,
+    value: value
+  })
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,23 +4,20 @@ import GlobalStyle from '../styles/global'
 import { useRouter } from 'next/dist/client/router';
 import { useEffect } from 'react';
 const isProduction = process.env.NODE_ENV === "production";
+import * as gtag from "../lib/gtag";
 
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter()
-  const handleRouteChange = (url) => {
-    window.gtag('config', 'G-WT5ZHZKW59', {
-      page_path: url,
-    });
-  };
-
   useEffect(() => {
-    router.events.on('routeChangeComplete', handleRouteChange);
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url);
     };
-  }, [router.events])
-
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return (
   <>
     <GlobalStyle />
